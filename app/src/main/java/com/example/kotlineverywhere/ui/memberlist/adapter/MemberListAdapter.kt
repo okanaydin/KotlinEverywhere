8fbbd17b0be1e8503a9367fad22c09e48f0b6dae
okanaydin <okan.aydin@isik.edu.tr>
2019-08-16 21:13:15 +0300

    Create Step.05

--- app/src/main/java/com/example/kotlineverywhere/ui/memberlist/adapter/MemberListAdapter.kt
+++ app/src/main/java/com/example/kotlineverywhere/ui/memberlist/adapter/MemberListAdapter.kt
@@ -4,15 +4,30 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.kotlineverywhere.R
+import com.example.kotlineverywhere.model.Member
 
 class MemberListAdapter : RecyclerView.Adapter<MemberListViewHolder>() {
 
+    var memberList: List<Member> = ArrayList()
+        set(value) {
+            field = value
+            this.notifyDataSetChanged()
+        }
+
+    private var onItemClickListener: ((Member?) -> Unit)? = null
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MemberListViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_member, parent, false)
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_member,parent,false)
         return MemberListViewHolder(itemView)
     }
 
-    override fun getItemCount(): Int = 0
+    override fun getItemCount(): Int = memberList.size
 
-    override fun onBindViewHolder(holder: MemberListViewHolder, position: Int) {}
+    override fun onBindViewHolder(holder: MemberListViewHolder, position: Int) {
+        holder.bind(memberList[position], onItemClickListener)
+    }
+
+    fun setOnItemClickListener(onItemClickListener: ((Member?) -> Unit)?) {
+        this.onItemClickListener = onItemClickListener
+    }
 }

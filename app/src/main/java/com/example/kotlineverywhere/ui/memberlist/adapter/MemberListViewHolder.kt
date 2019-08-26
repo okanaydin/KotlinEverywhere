4c46155560154ca2389d8c2b44b55da191c783dd
Sertac Okan Celik <sertacokan@gmail.com>
2019-08-26 19:41:07 +0300

    Modify MemberListViewModel , MemberListAdapter and MemberListFragment

--- app/src/main/java/com/example/kotlineverywhere/ui/memberlist/adapter/MemberListViewHolder.kt
+++ app/src/main/java/com/example/kotlineverywhere/ui/memberlist/adapter/MemberListViewHolder.kt
@@ -1,9 +1,22 @@
 package com.example.kotlineverywhere.ui.memberlist.adapter
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.kotlineverywhere.R
+import com.example.kotlineverywhere.model.Member
 
 class MemberListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
-    fun bind() {}
+    companion object{
+        fun getMemberListViewHolder(parent: ViewGroup):MemberListViewHolder{
+            val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_member, parent, false)
+            return MemberListViewHolder(itemView)
+        }
+    }
+
+    fun bind(member:Member) {
+
+    }
 }

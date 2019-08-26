89e3bd68657bb8bfb3fa33059ab63dbc4d7ff1e0
Sertac Okan Celik <sertacokan@gmail.com>
2019-08-26 18:57:07 +0300

    Seperate viewholder creation from adapter

--- app/src/main/java/com/example/kotlineverywhere/ui/memberlist/adapter/MemberListViewHolder.kt
+++ app/src/main/java/com/example/kotlineverywhere/ui/memberlist/adapter/MemberListViewHolder.kt
@@ -1,9 +1,19 @@
 package com.example.kotlineverywhere.ui.memberlist.adapter
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.kotlineverywhere.R
 
 class MemberListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    companion object{
+        fun getMemberListViewHolder(parent:ViewGroup) : MemberListViewHolder{
+            val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_member, parent, false)
+            return MemberListViewHolder(itemView)
+        }
+    }
 
     fun bind() {}
 }
